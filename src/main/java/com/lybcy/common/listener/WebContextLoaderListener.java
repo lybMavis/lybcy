8589360e2cdb320538aecc38eafef215d8c38d9e
liangyongbiao <liangyongbiao.com.cn>
2017-02-16 21:01:39 +0800

    【add】增加公共包common,以及新建StringUtil类。

---
+++ src/main/java/com/lybcy/common/listener/WebContextLoaderListener.java
@@ -0,0 +1,9 @@
+package com.lybcy.common.listener;
+
+import javax.servlet.ServletContextListener;
+
+/**
+ * Created by liangyongbiao on 2017/2/16.
+ */
+public class WebContextLoaderListener{//implements ServletContextListener {
+}
---
+++ src/main/java/com/lybcy/common/util/StringUtils.java
@@ -0,0 +1,14 @@
+package com.lybcy.common.util;
+
+/**
+ * Created by liangyongbiao on 2017/2/16.
+ */
+public class StringUtils extends org.apache.commons.lang3.StringUtils{
+
+
+    public static void main(String[] args) {
+        String str = "";
+        System.out.println(isEmpty(str));
+    }
+
+}
